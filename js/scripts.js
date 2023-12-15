2b050017bf53dcb4925ada38aa3fcfdb57f53655
vitor-developer <vitordeveloper5@gmail.com>
2023-12-14 21:14:01 -0300

    ALTERAÇA NA PAGINA INDEX DO PROJETO  - INSERIDO IMAGENS  - INSERIDO DESCRIÇÃO  - INSERIDO TITULO  - CONEXAO COM DEMAIS PAGINAS

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,31 @@ function changeImage() {
     images[i].style.display = 'block';
 }
 
-setInterval(changeImage, 10000); // Troca a imagem a cada 10 segundos
+setInterval(changeImage, 5000); 
 
-// Alteração no JavaScript
 document.addEventListener('DOMContentLoaded', function() {
-    var menuHorizontal = document.getElementById('menu-horizontal');
+    var menuHorizontal = document.getElementById('navegacao');
     var menuVertical = document.getElementById('menu-vertical');
 
     menuHorizontal.addEventListener('click', function(event) {
         if (event.target.tagName === 'A' && event.target.parentNode.classList.contains('menu-option')) {
             var position = event.target.getBoundingClientRect();
-            menuVertical.style.display = 'block';
-            menuVertical.style.left = position.left + 'px';
-            menuVertical.style.top = position.top + position.height + 'px';
+            // Toggle visibility of the vertical menu
+            menuVertical.style.display = menuVertical.style.display === 'block' ? 'none' : 'block';
+            // Adjust position only if the menu is displayed
+            if (menuVertical.style.display === 'block') {
+                menuVertical.style.left = position.left + 'px';
+                menuVertical.style.top = position.top + position.height + 'px';
+            }
         } else {
+            // Hide the vertical menu
             menuVertical.style.display = 'none';
         }
     });
 
     menuVertical.addEventListener('click', function(event) {
         if (event.target.tagName === 'A') {
+            // Hide the vertical menu when a link is clicked
             menuVertical.style.display = 'none';
         }
     });
